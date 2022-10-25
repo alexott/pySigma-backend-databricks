62856bc8b1f885eadffc25513643b6aab0b74116
Julian Shalaby <Julian.shalaby@databricks.com>
2022-10-25 07:13:41 -0700

    improved sigma link parser

--- sigma/backends/databricks/SigmaLinkParser.py
+++ sigma/backends/databricks/SigmaLinkParser.py
@@ -5,20 +5,27 @@ import urllib.request
 import re
 
 
-def parse_sigma_directory(directory):
+def parse_sigma_link(link):
     parser = 'html.parser'
-    resp = urllib.request.urlopen(directory)
+    resp = urllib.request.urlopen(link)
     soup = BeautifulSoup(resp, parser, from_encoding=resp.info().get_param('charset'))
 
     sigma_rules = []
-    for link in soup.find_all('a', href=True):
-        if re.search("/SigmaHQ/sigma/blob/master/rules", link['href']) is not None:
-            try:
-                link = f"https://raw.githubusercontent.com{link['href'].replace('/blob', '')}"
-                rule = SigmaRule(requests.get(link).text)
-                sigma_rules.append(rule)
-            except Exception as e:
-                print(f"Bad rule: {link}")
-                print(e)
+    if re.search("github\\.com/SigmaHQ/sigma/blob/master/rules/*/*/*", link) is not None:
+        sigma_rules.append(
+            SigmaRule(requests.get(link.replace('/blob', '').replace('github', 'raw.githubusercontent')).text)
+        )
+    elif re.search("github\\.com/SigmaHQ/sigma/tree/master/rules/*/*", link) is not None:
+        for x in soup.find_all('a', href=True):
+            if re.search("/SigmaHQ/sigma/blob/master/rules", x['href']) is not None:
+                try:
+                    x = f"https://raw.githubusercontent.com{x['href'].replace('/blob', '')}"
+                    rule = SigmaRule(requests.get(x).text)
+                    sigma_rules.append(rule)
+                except Exception as e:
+                    print(f"Bad rule: {x}")
+                    print(e)
+    elif re.search("raw\\.githubusercontent\\.com/SigmaHQ/sigma/master/rules/*/*/*", link) is not None:
+        sigma_rules.append(SigmaRule(requests.get(link).text))
 
     return sigma_rules
--- tests/test_sigma_link_parser.py
+++ tests/test_sigma_link_parser.py
@@ -1,7 +1,19 @@
 from sigma.backends.databricks.SigmaLinkParser import *
 
 
-def test_databricks_sigma_and_expression():
+def test_sigma_parse_directory():
     link = "https://github.com/SigmaHQ/sigma/tree/master/rules/cloud/aws"
-    for rule in parse_sigma_directory(link):
+    for rule in parse_sigma_link(link):
+        print(rule.title)
+
+
+def test_sigma_parse_link():
+    link = "https://github.com/SigmaHQ/sigma/blob/master/rules/cloud/aws/aws_cloudtrail_disable_logging.yml"
+    for rule in parse_sigma_link(link):
+        print(rule.title)
+
+
+def test_sigma_parse_raw_link():
+    link = "https://raw.githubusercontent.com/SigmaHQ/sigma/master/rules/cloud/aws/aws_cloudtrail_disable_logging.yml"
+    for rule in parse_sigma_link(link):
         print(rule.title)
